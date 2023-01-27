37eb5219fe0a3dac96d3607ffd8a563debca2ed6
Murilo-MRS <murilosantana1993@gmail.com>
2023-01-27 12:12:37 -0300

    feat: req 1 - migrations - blogposts

--- src/models/BlogPost.model.js
+++ src/models/BlogPost.model.js
@@ -1,7 +1,7 @@
 module.exports = (sequelize, DataTypes) => {
   const BlogPost = sequelize.define('BlogPost', {
-    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     title: DataTypes.STRING,
+    content: DataTypes.STRING,
     userId: { type: DataTypes.INTEGER, foreignKey: true },
     published: DataTypes.DATE,
     updated: DataTypes.DATE,
