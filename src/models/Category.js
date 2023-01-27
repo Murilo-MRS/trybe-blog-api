ac583a957377a8adb9a5958ec1eaf2f13fd69359
Murilo-MRS <murilosantana1993@gmail.com>
2023-01-27 17:45:14 -0300

    fix: add id nos models

--- src/models/Category.js
+++ src/models/Category.js
@@ -1,5 +1,6 @@
 module.exports = (sequelize, DataTypes) => {
   const Category = sequelize.define('Category', {
+    id: { type: DataTypes.INTEGER, primaryKey: true },
     name: DataTypes.STRING,
   },
   {
--- src/models/User.js
+++ src/models/User.js
@@ -1,6 +1,8 @@
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
+    id: { type: DataTypes.INTEGER, primaryKey: true },
     displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
     password: DataTypes.STRING,
     image: DataTypes.STRING,
   },
