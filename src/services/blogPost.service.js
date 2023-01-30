1fe4a0331d492334f44062ef69df483abe5c6b1c
Murilo-MRS <murilosantana1993@gmail.com>
2023-01-30 11:23:53 -0300

    fix: req 13 - retorno do endpoint GET /post

--- src/services/blogPost.service.js
+++ src/services/blogPost.service.js
@@ -4,7 +4,7 @@ const getAllBlogPost = async () => {
   const blogPosts = await BlogPost.findAll({
     include: [
       { model: User, as: 'user', attributes: { exclude: ['password'] } },
-      { model: Category, as: 'categories' },
+      { model: Category, as: 'categories', through: { attributes: [] } },
   ],
   });
 
